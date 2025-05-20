ca7481260fb02df7db387c1a763ba9ec40f48de3
Ria Balli <35355202+rb0000@users.noreply.github.com>
2025-05-20 16:33:11 -0400

    done (#165)
    Co-authored-by: Ria Balli <ria@credal.ai>

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 export { runAction, getActionGroups, type ActionGroupsReturn } from "./app";
 export { ACTION_GROUPS, type ActionGroups } from "./actions/groups";
 export { ActionMapper } from "./actions/actionMapper";
+export { ActionTemplate } from "./actions/parse";
 
 export * from "./actions/autogen/templates";
 export * from "./actions/autogen/types";
